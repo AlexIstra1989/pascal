914f5d2d31c42491e4b72e0097d0b2a014324115
AlexIstra1989 <alex888fcsm@gmail.com>
2023-02-19 22:29:34 +0300

    added services block

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,8 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+// if (window.innerWidth <= 992) {
+//  document.getElementById("tab-img-1").src = "img/services/tab-1.2.png";
+//  document.getElementById("tab-img-2").src = "img/services/tab-2.2.png";
+// }

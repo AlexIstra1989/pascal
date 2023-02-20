6a7a2f5c8a54955d60acbe914aaba9acc157e8a1
AlexIstra1989 <alex888fcsm@gmail.com>
2023-02-20 21:37:57 +0300

    added prices block

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -3,7 +3,12 @@ import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
-// if (window.innerWidth <= 992) {
-//  document.getElementById("tab-img-1").src = "img/services/tab-1.2.png";
-//  document.getElementById("tab-img-2").src = "img/services/tab-2.2.png";
-// }
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) { /* если страница прокручена хотя бы на 1 пиксель */
+    header.classList.add('scrolled'); /* добавляем класс, задающий новый фон */
+  } else {
+    header.classList.remove('scrolled'); /* удаляем класс, возвращающий прозрачный фон */
+  }
+});
